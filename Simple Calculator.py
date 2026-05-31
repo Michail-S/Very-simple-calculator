814535f9f7b5b1e46b6c4efd31f0c2fb3deb1b38
Michail-S <m.studenikin@proton.me>
2026-05-31 08:50:31 +0200

    Added ability to do another calculation without program restart

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -13,61 +13,79 @@ skip_loading = False
 print("Welcome")
 loading(1)
 
-# Operation type selection
 while True:
-  operation = input("What operation do you want to perform? (+, -, /, *): ").lower().strip()
 
-  if operation == "plus" or operation == "+":
-      operation = "+"
-      break
-  elif operation == "minus" or operation == "-":
-      operation = "-"
-      break
-  elif operation == "divide" or operation == "/":
-      operation = "/"
-      break
-  elif operation == "multiply" or operation == "*":
-      operation = "*"
-      break
+  # Operation type selection
+  while True:
+    operation = input("What operation do you want to perform? (+, -, /, *): ").lower().strip()
+
+    if operation == "plus" or operation == "+":
+        operation = "+"
+        break
+    elif operation == "minus" or operation == "-":
+        operation = "-"
+        break
+    elif operation == "divide" or operation == "/":
+        operation = "/"
+        break
+    elif operation == "multiply" or operation == "*":
+        operation = "*"
+        break
+    else:
+        print("Invalid operation. Please enter one of the following types: +, -, /, *")
+
+  print(f"Current calculation: _ {operation} _")
+
+  # First number selection
+  while True:
+      try:
+          first_number = float(input("Enter the first number for your calculation: "))
+          break
+      except ValueError:
+          print("Invalid number. Please enter a valid number.")
+
+  print(f"Current calculation: {first_number} {operation} _")
+
+  # Second number selection
+  while True:
+      try:
+          second_number = float(input("Enter the second number for your calculation: "))
+          break
+      except ValueError:
+          print("Invalid number. Please enter a valid number.")
+
+  print(f"Current calculation: {first_number} {operation} {second_number}")
+  print("Calculating...")
+  loading(0.5)
+
+  # Do the calculation and print the result
+  if operation == "+":
+      print(f"Here is the result: {first_number + second_number}")
+  elif operation == "-":
+      print(f"Here is the result: {first_number - second_number}")
+  elif operation == "/":
+      print(f"Here is the result: {first_number / second_number}")
+  elif operation == "*":
+      print(f"Here is the result: {first_number * second_number}")
   else:
-      print("Invalid operation. Please enter one of the following types: +, -, /, *")
+      print("A critical error accured, exiting...")
 
-print(f"Current calculation: _ {operation} _")
-
-# First number selection
-while True:
-    try:
-        first_number = float(input("Enter the first number for your calculation: "))
-        break
-    except ValueError:
-        print("Invalid number. Please enter a valid number.")
-
-print(f"Current calculation: {first_number} {operation} _")
-
-# Second number selection
-while True:
-    try:
-        second_number = float(input("Enter the second number for your calculation: "))
-        break
-    except ValueError:
-        print("Invalid number. Please enter a valid number.")
-
-print(f"Current calculation: {first_number} {operation} {second_number}")
-print("Calculating...")
-loading(0.5)
-
-# Do the calculation and print the result
-if operation == "+":
-    print(f"Here is the result: {first_number + second_number}")
-elif operation == "-":
-    print(f"Here is the result: {first_number - second_number}")
-elif operation == "/":
-    print(f"Here is the result: {first_number / second_number}")
-elif operation == "*":
-    print(f"Here is the result: {first_number * second_number}")
-else:
-    print("A critical error accured, exiting...")
-
+  
+  # Gives the option to perform another calculation without restarting the whole program
+  while True:
+    another_calculation = input("Do you want to perform another calculation? (yes, no): ").lower()
+    if another_calculation == "yes" or another_calculation == "y":
+      break
+    elif another_calculation == "no" or another_calculation == "n":
+      print("Bye!")
+      break
+    else:
+      print("Please enter yes or no!")
+      continue
+  if another_calculation == "yes" or another_calculation == "y":
+    continue
+  else:
+    break
+  
 
 # Made by Michail
-
